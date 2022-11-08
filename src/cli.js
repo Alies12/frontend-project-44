ec7c72e1e3f9bfa6b9b98ee014b6aa701d643400
Andrey <singaevskiy2001@mail.ru>
2022-11-08 13:59:59 +0300

    fix name

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('Your answer: ');
-  return console.log(`Hello ${name}!`);
+  const username = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${username}!`);
 };
